3c41bfc87d1a9201d926d4fbb0666bda5f39225d
Maksym-Zhuravel <allwegotizus1@gmail.com>
2024-05-20 22:04:59 +0300

    update project

--- src/components/articles-api.js
+++ src/components/articles-api.js
@@ -4,16 +4,16 @@ axios.defaults.baseURL = "https://api.unsplash.com/";
 const ACCESS_KEY = "4yKS7ti03CDkZBSmRWli3PGmFjeATL3yf--1shu4VWY";
 
 export const getImages = async (topic) => {
-  const response = await axios.get(
-    `search/photos?client_id=${ACCESS_KEY}&page=1&query=${topic}`
-  );
+  // const response = await axios.get(
+  //   `search/photos?client_id=${ACCESS_KEY}&page=1&query=${topic}`
+  // );
 
-  // const response = await axios.get("/search/photos", {
-  //   params: {
-  //     client_id: { ACCESS_KEY },
-  //     page: 1,
-  //     query: topic,
-  //   },
-  // });
+  const response = await axios.get(`search/photos`, {
+    params: {
+      client_id: ACCESS_KEY,
+      page: 1,
+      query: topic,
+    },
+  });
   return response.data.results;
 };
