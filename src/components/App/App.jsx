8ff21c0056b011cb60bd2c093f510ca38d553d0b
Maksym-Zhuravel <allwegotizus1@gmail.com>
2024-05-21 21:56:28 +0300

    update project

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,7 @@
 import SearchBar from "../SearchBar/SearchBar";
 import ImageGallery from "../ImageGallery/ImageGallery";
 import Loader from "../Loader/Loader";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { getImages } from "../articles-api";
 import ErrorMessage from "./ErrorMessage/ErrorMessage";
 import LoadMoreBtn from "../LoadMoreBtn/LoadMoreBtn";
@@ -10,19 +10,37 @@ export default function App() {
   const [images, setImages] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    if (query.trim() === "") {
+      return;
+    }
+    async function fetchImages() {
+      try {
+        setError(false);
+        setLoading(true);
+        const data = await getImages(query, page);
+        setImages((prevImages) => [...prevImages, ...data]);
+      } catch (error) {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchImages();
+  }, [page, query]);
 
   const handleSearch = async (newImages) => {
-    try {
-      setError(false);
-      setLoading(true);
-      setImages([]);
-      const data = await getImages(newImages);
-      setImages(data);
-    } catch (error) {
-      setError(true);
-    } finally {
-      setLoading(false);
-    }
+    setQuery(newImages);
+    setPage(1);
+    setImages([]);
+  };
+
+  const handleLoadMore = async () => {
+    setPage(page + 1);
   };
 
   return (
@@ -31,7 +49,7 @@ export default function App() {
       {error && <ErrorMessage />}
       {images.length > 0 && <ImageGallery images={images} />}
       {loading && <Loader />}
-      <LoadMoreBtn />
+      <LoadMoreBtn onClick={handleLoadMore} />
     </>
   );
 }
--- src/components/articles-api.js
+++ src/components/articles-api.js
@@ -3,16 +3,13 @@ import axios from "axios";
 axios.defaults.baseURL = "https://api.unsplash.com/";
 const ACCESS_KEY = "4yKS7ti03CDkZBSmRWli3PGmFjeATL3yf--1shu4VWY";
 
-export const getImages = async (topic) => {
-  // const response = await axios.get(
-  //   `search/photos?client_id=${ACCESS_KEY}&page=1&query=${topic}`
-  // );
-
+export const getImages = async (topic, currentPage) => {
   const response = await axios.get(`search/photos`, {
     params: {
       client_id: ACCESS_KEY,
-      page: 1,
+      page: currentPage,
       query: topic,
+      per_page: 8,
     },
   });
   return response.data.results;
